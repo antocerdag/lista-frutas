e95aefde15c068891ce06a814039f852868ea946
Antonia Cerda <antoniacerdag@gmail.com>
2017-04-20 17:48:50 -0300

    Terminado el botón, estilos y html

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,16 +5,15 @@ var records = document.getElementById('records');
 // Constructor para generar un nuevo producto
 function Producto(compra) {
   this.compra = compra.toLowerCase();
-  this.producttID = (productos.length + 1);  
+  this.producttID = (productos.length + 1);
 };
 
-// Cuando hacen click en el boton de nueva compra, crea una nueva compra y la añade al array de productos
+// Cuando hacen click en el boton , se añade al array de productos
 var addCompra = document.getElementById('print');
 addCompra.onclick = function() {
-  var product  = new Producto (document.getElementById('print'));
+  var product  = new Producto (compra);
   productos.push(product);
   printHTML(product.toHTML());
-  printHTML(mergeHTML());
 };
 
 
@@ -25,12 +24,25 @@ Producto.prototype.toHTML = function () {
   return html;
 }
 
-
+//Función que une todas las compras guardadas en el array productos
+function mergeHTML (){
+  var html = '';
+  for (var i=0; i<productos.length; i++){
+    html += productos[i].toHTML();
+  }
+  return html;
+}
 
 //función que imprime un producto luego de ingresarlo
 function printHTML (html){
   records.innerHTML = '';
   records.innerHTML = html;
+}
+
+
+
+// al hacerle click a mostrar todas las compras, imprime todo lo que está en el array
+  printHTML(mergeHTML());
 }
 
 
